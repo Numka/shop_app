efdda18f00079f6bac1d6924a7e0206acee9c04a
Numinor <numinor93@mail.ru>
2020-11-30 01:49:33 +0300

    Created a form to submit a new product

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './edit_product_screen.dart';
+import '../widgets/app_drawer.dart';
+import '../widgets/user_product_item.dart';
+import '../providers/products.dart';
 
 class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your products'),
+        title: const Text('Your products'),
         actions: [
           IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {},
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           ),
         ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
       ),
     );
   }
