c91cd35476b32fa8e1230daf15ec0084b999ed09
Numinor <numinor93@mail.ru>
2020-11-29 21:20:43 +0300

    Polished orders functionality

--- lib/widgets/order_item_card.dart
+++ lib/widgets/order_item_card.dart
@@ -1,12 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../providers/orders.dart';
 
-class OrderItemCard extends StatelessWidget {
+class OrderItemCard extends StatefulWidget {
   final OrderItem order;
 
   OrderItemCard(this.order);
+
+  @override
+  _OrderItemCardState createState() => _OrderItemCardState();
+}
+
+class _OrderItemCardState extends State<OrderItemCard> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +24,46 @@ class OrderItemCard extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd MM yyyy:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 20.0 + 10.0, 180.0),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (product) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '${product.quantity}x \$${product.price}',
+                            style: TextStyle(fontSize: 18, color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
